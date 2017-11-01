8c31d746acd503a08623197cf6b6d471a8916409
Saeyoung Macx Kim <remsimple93@users.noreply.github.com>
2017-11-02 08:47:09 +0900

    changed analog input reading algorithm
    performance enhancement of sampling time from 2 s to 0.5 s

--- Pip_testbed/Pip_testbed.ino
+++ Pip_testbed/Pip_testbed.ino
@@ -9,27 +9,36 @@
 #include <Wire.h>
 #include "RTClib.h"
 
-
 // Variable declaration
 int pHreadout = A0;
+int pHtotal = 0;
 int DOreadout = A1;
+int DOtotal = 0;
 int Tempreadout = A2;
+int Temptotal = 0;
+int i = 0;                           // counter for quick successive analog input readings
 float pHvoltage;
 float DOvoltage;
 float Tempvoltage;
-int ID = 1;                 // number of data entries in one file
+int ID = 1;                          // number of data entries in one file
 const int chipSelect = 10;
 File dataFile;
-float temperatureC;         // Temperature in Celcius
-String dataString;          // data entry for experiments
-String CurrentTime;         // for making file names
-char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}; // for RTC to refer to
-long elapsedTime;           // to be used as timer during experiments
-long startTime;
+float temperatureC;                  // Temperature in Celcius
+String dataString;                   // data entry for experiments
+String CurrentTime;                  // for making file names
+long elapsedTime;           // legacy code
+float con_elapsedMillis;             // elapsedMillis converted to seconds
+unsigned long startTime;             // legacy code
+unsigned long startMillis;           // starting millisecond timestamp
+unsigned long elapsedMillis;         // elapsed millisecond for timestamping
+unsigned long previousMillis;        // previous millisecond for deciding if or not to proceed with next void loop
+unsigned long currentMillis;         // current millisecond for deciding if or not to proceed with next void loop
+unsigned long samplingTime = 500;   // sampling time in milliseconds, currently set to 1 second, keep it above 330
 String fileName;
-char pHbuff[9];
-char DObuff[9];
-char tempbuff[7];
+char pHbuff[6];
+char DObuff[6];
+char tempbuff[6];
+char timebuff[8];
 String currentMonth;
 String currentDay;
 String currentHour;
@@ -106,13 +115,13 @@ void setup() {
 
     Serial.println(F("RTC initialization... DONE."));
     lcd.clear();
-    lcd.print(F("RTC initialization... DONE."));
+    lcd.print(F("RTC ready!"));
     delay(1000);
 
     // define DateTime and CurrentTime string variable (call entire date and time atm, we'll cut it up later)
     DateTime now = rtc.now();
 
-    // DS1307 output formatting fix
+    // DS1307 output formatting fix --------------------------------------------------------------------
     if ( now.month() < 10 ){
       currentMonth = "0" + String(now.month());
     }
@@ -186,9 +195,15 @@ void setup() {
     lcd.print(F("SD card ready!"));
     delay(1000);
     
-    // define String fileName variable and print to SM
+    // define String fileName variable and print to SM and LCD
     fileName = currentMonth + currentDay + currentHour + currentMin;
     Serial.println("File name is " + fileName);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print(F("Filename:"));
+    lcd.setCursor(0, 1);
+    lcd.print(fileName);
+    delay(5000);
 
     // print to LCD
     lcd.clear();
@@ -204,7 +219,7 @@ void setup() {
     delay(1000);
 
     // Initial time when starting experiment
-    startTime = now.unixtime() + 4;
+    startTime = now.unixtime() + 9;
     Serial.println("Starting unixtime: " + String(startTime));
 
     // open Data File I will be working with and write header for identification
@@ -223,18 +238,67 @@ void setup() {
     else {
       Serial.println(F("File FAILED to open."));
     }
-  
+
+    // timestamp start 
+    startMillis = millis();
+    
 }
 
 // -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 void loop() {
 
+  // timestamp for sampling time
+  previousMillis = millis();
+
   // read from analog pins and convert ---------------------------------------------------------------
     // get voltage reading from A0 (pH), A1 (DO), and A2 (Temp.)
-    int pHreadout = analogRead(A0);
-    int DOreadout = analogRead(A1);
-    int Tempreadout = analogRead(A2);
+    // initial ping
+    analogRead(A0);
+    delay(10);
+
+    // get 10 readings of pHreadout  ---------------------------------------------------------------
+    for(i = 0; i < 10; i++)
+      {
+        pHreadout = analogRead(A0);
+        pHtotal = pHtotal + pHreadout; // add new pHreadout to old pHreadout, do this from count 0 to 9
+        delay(10);
+      }
+
+      pHreadout = pHtotal / 10; // avg of 10 readings
+      pHtotal = 0;
+      i = 0;                    // reinitialize i as 0 
+
+    // initial ping    
+    analogRead(A1); // initial ping
+    delay(10);
+
+    // get 10 readings of DOreadout  ---------------------------------------------------------------
+    for(i = 0; i < 10; i++)
+      {
+        DOreadout = analogRead(A1);
+        DOtotal = DOtotal + DOreadout; // add new DOreadout to old DOreadout, do this from count 0 to 9
+        delay(10);
+      }
+
+      DOreadout = DOtotal / 10; // avg of 10 readings
+      DOtotal = 0;
+      i = 0;                    // reinitialize i as 0 
+
+    // initial ping  
+    analogRead(A2);
+    delay(10);
+
+    // get 10 readings of Tempreadout  ---------------------------------------------------------------
+    for(i = 0; i < 10; i++){
+        Tempreadout = analogRead(A2);
+        Temptotal = Temptotal + Tempreadout; // add new Tempreadout to old Tempreadout, do this from count 0 to 9
+        delay(10);
+      }
+
+      Tempreadout = Temptotal / 10; // avg of 10 readings
+      Temptotal = 0;
+      i = 0;                        // reinitialize i as 0 
 
     // convert reading to voltage
     float pHvoltage = pHreadout * 5.0/1024;
@@ -243,9 +307,6 @@ void loop() {
 
     // convert voltage to temperature, converting from 10 mV per degree with 500 mV offset to temperature
     float temperatureC = (Tempvoltage - 0.5) * 100;
-
-    // print to serial monitor for troubleshooting
-    // Serial.print(String("pH Voltage: ") + String(pHvoltage) + String(" V") + String(", ") + String("DO voltage: ") + String(DOvoltage) + String(" V") + String(", ") + String("Temp: " + String(temperatureC)) + String(" C") + String(", "));
 
   // writing to LCD ----------------------------------------------------------------------------------
 
@@ -256,31 +317,44 @@ void loop() {
     lcd.setCursor(0, 0);
 
     // print pHvoltage on LCD
-    lcd.print(F("pH V: "));
-    lcd.print(pHvoltage, 6);
+    lcd.print(F("pH: "));
+    lcd.print(pHvoltage, 3);
     lcd.print(F(" V"));
 
     // put cursor on second row first column
     lcd.setCursor(0, 1);
 
     // print DOvoltage on LCD
-    lcd.print(F("DO V: "));
-    lcd.print(DOvoltage, 6);
+    lcd.print(F("DO: "));
+    lcd.print(DOvoltage, 3);
     lcd.print(F(" V"));
 
   // writing to microSD card module -------------------------------------------------------------------
 
-    // convert float variables to strings using dtostrf
-    dtostrf(pHvoltage, 8, 6, pHbuff);
-    dtostrf(DOvoltage, 8, 6, DObuff);
-    dtostrf(temperatureC, 6, 2, tempbuff);
+    // convert sensor readings float variables to strings using dtostrf
+    dtostrf(pHvoltage, 5, 3, pHbuff);
+    dtostrf(DOvoltage, 5, 3, DObuff);
+    dtostrf(temperatureC, 5, 1, tempbuff);
 
-    // calculate elapsed time since start of power
-    DateTime now = rtc.now();
-    elapsedTime = now.unixtime() - startTime;
+    // calculate elapsed time since start of power, legacy code
+    // DateTime now = rtc.now();
+    // elapsedTime = now.unixtime() - startTime;
 
+    // calculate elapsed millisecond since startMillis timestamp
+    elapsedMillis = millis() - startMillis;
+    con_elapsedMillis = elapsedMillis * 0.001; // multiply elapsed millisecond with 0.001 to convert to seconds
+
+    // convert elapsed time float variables to strings using dtostrf
+    dtostrf(con_elapsedMillis, 7, 3, timebuff);
+
+    // print seconds on LCD
+    lcd.setCursor(12, 0);
+    lcd.print(con_elapsedMillis, 0);
+    lcd.setCursor(12, 1);
+    lcd.print(F("s"));
+    
     // create data string to write to SD card
-    dataString = String(ID) + ", " + String(elapsedTime) + ", " + pHbuff + ", " + DObuff + "," + tempbuff;
+    dataString = String(ID) + ", " + timebuff + ", " + pHbuff + ", " + DObuff + "," + tempbuff;
     
     // open Data File I will be working with
     File dataFile = SD.open(fileName + ".csv", FILE_WRITE);
@@ -302,5 +376,11 @@ void loop() {
     // increment ID number
     ID++;
 
-  delay(2000);
+  // wait until samplingTime is acheived, then move onto next void loop
+  currentMillis = millis();
+  while(currentMillis - previousMillis < samplingTime){
+    delay(10);
+    currentMillis = millis();
+  }
+  
 }
